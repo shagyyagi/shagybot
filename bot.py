45475f27c5545f7e6bd48318fd55d8c5a467b5e7
shagyyagi <ihediohaprincewill@gmail.com>
2026-07-04 18:39:48 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -495,7 +495,7 @@ class NigerianAccountBot:
 # ============================================
 
 target_url = "https://nnnrc.com/#/register"
-NUM_ACCOUNTS = 2
+NUM_ACCOUNTS = 3
 
-bot = NigerianAccountBot(start_code=3095655)
+bot = NigerianAccountBot(start_code=3095665)
 bot.run(target_url, num_accounts=NUM_ACCOUNTS)
